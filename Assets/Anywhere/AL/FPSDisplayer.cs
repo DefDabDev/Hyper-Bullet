d19512aa321ce8eb6b76f3c5d3b0ac33050f6056
Kim Deokwon <64bit_decryption@naver.com>
2017-03-30 16:24:16 +0900

    Add UI Base
    YEAH

---
+++ Assets/Anywhere/AL/FPSDisplayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AL
+{
+    [ExecuteInEditMode]
+    public class FPSDisplayer : MonoBehaviour
+    {
+
+        public Color fontColor = Color.white;
+
+        float deltaTime = 0.0f;
+
+        void Update()
+        {
+            deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        }
+
+        void OnGUI()
+        {
+            int w = Screen.width, h = Screen.height;
+
+            GUIStyle style = new GUIStyle();
+
+            Rect rect = new Rect(0, 0, w, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 2 / 30;
+            style.normal.textColor = fontColor;
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
+        }
+    }
+}
