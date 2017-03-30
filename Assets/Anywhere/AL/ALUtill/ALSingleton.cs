0ef3afc3aa774f06a8f219049f1e82e3fc2b1c01
Kim Deokwon <64bit_decryption@naver.com>
2017-03-30 10:33:44 +0900

    Add InGame Sprites, ALUtil
    Base Work

---
+++ Assets/Anywhere/AL/ALUtill/ALSingleton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AL.ALUtil
+{
+    public class ALSingleton<T> where T : ALSingleton<T>, new()
+    {
+        static T _instance = null;
+        /// <summary>
+        /// Singleton객체를 리턴 함
+        /// </summary>
+        public static T instance
+        {
+            get
+            {
+                return _instance ?? (_instance = new T());
+            }
+        }
+    }
+}
---
+++ Assets/Anywhere/AL/ALUtill/ALSingletonComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AL.ALUtil
+{
+    public class ALSingletonComponent<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        static T _instance = null;
+        /// <summary>
+        /// Singleton객체를 리턴 함
+        /// </summary>
+        public static T instance { get { return _instance ?? getInstanceObject(); } }
+
+        void Awake()
+        {
+            _instance = this as T;
+        }
+
+        static T getInstanceObject()
+        {
+            _instance = GameObject.FindObjectOfType<T>().GetComponent<T>();
+            return _instance ?? null;
+        }
+    }
+}
