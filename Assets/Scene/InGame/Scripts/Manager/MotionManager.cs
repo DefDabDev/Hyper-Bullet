941aa649aaab39d7d26adb8574eac55a9d2acd37
kyechan99 <kyechan99@naver.com>
2017-06-18 21:10:12 +0900

    add BulletSelectUI

---
+++ Assets/Scene/InGame/Scripts/Manager/MotionManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GM
+{
+    public class MotionManager : MonoBehaviour
+    {
+        [SerializeField]
+        Animator bulletInterface;
+
+        float prevDist = 0;
+        float curDist = 0;
+        float rateChange = 0;
+
+        // 줌 In/Out 방식
+        //void Update()
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Z))
+        //        bulletInterface.SetBool("OnOff", true);
+        //    if (Input.GetKeyDown(KeyCode.X))
+        //        bulletInterface.SetBool("OnOff", false);
+
+        //    if (Input.touchCount.Equals(2))
+        //    {
+        //        if (Input.GetTouch(0).phase.Equals(TouchPhase.Moved) &&
+        //            Input.GetTouch(1).phase.Equals(TouchPhase.Moved))
+        //        {
+        //            curDist = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+        //            rateChange = prevDist - curDist;
+        //            if (rateChange > 150)
+        //                bulletInterface.SetBool("OnOff", true);
+        //            else if (rateChange < -150)
+        //                bulletInterface.SetBool("OnOff", false);
+        //            prevDist = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
+        //        }
+        //        else
+        //        {
+        //            curDist = 0;
+        //            prevDist = 0;
+        //            rateChange = 0;
+        //        }
+        //    }
+        //    else if (Input.touchCount.Equals(1))
+        //    {
+        //        curDist = 0;
+        //        prevDist = 0;
+        //        rateChange = 0;
+        //        Debug.Log(Input.GetTouch(0).position);
+        //    }
+        //}
+
+        /// <summary>
+        /// 총알 선택 UI 창 On/Off (선택 방식)
+        /// </summary>
+        public void checkSelectBulletUI()
+        {
+            bulletInterface.SetBool("OnOff", bulletInterface.GetBool("OnOff") ? false : true);
+        }
+    }
+}
